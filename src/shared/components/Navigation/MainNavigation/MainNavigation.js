61444e5bcf714847f37c47f93584c53fbb0f1a47
Javascript Jedi <nithin.thewebdev@gmail.com>
2021-03-07 14:41:06 +0530

    added animations to sidedrawer

--- src/shared/components/Navigation/MainNavigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation/MainNavigation.js
@@ -7,26 +7,28 @@ import BackDrop from "../../UIElements/BackDrop/BackDrop";
 import "./MainNavigation.css";
 const MainNavigation = (props) => {
   const [drawerIsOpen, setDrawerIsOpen] = useState(false);
-  const openDrawer = () => {
+  const openDrawerHandler = () => {
     setDrawerIsOpen(true);
   };
-
-  const closeDrawer = () => {
+  const closeDrawerHandler = () => {
     console.log("close backdrop");
     setDrawerIsOpen(false);
   };
   return (
     <>
-      {drawerIsOpen && <BackDrop onClick={closeDrawer} />}
-      {drawerIsOpen && (
-        <SideDrawer>
-          <nav className='main-navigation__drawer-nav'>
-            <NavLinks />
-          </nav>
-        </SideDrawer>
-      )}
+      {drawerIsOpen && <BackDrop onClick={closeDrawerHandler} />}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
       <MainHeader>
-        <button className='main-navigation__menu-btn' onClick={openDrawer}>
+        <button
+          className='main-navigation__menu-btn'
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
--- src/shared/components/Navigation/SideDrawer/SideDrawer.js
+++ src/shared/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,10 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./SideDrawer.css";
-
+import { CSSTransition } from "react-transition-group";
 const SideDrawer = (props) => {
-  const content = <aside className='side-drawer'>{props.children}</aside>;
+  const content = (
+    // in={props.show} - tell sidebar when the sidebar is visible
+    // classNames - special classname for CSSTransition
+    // mountOnEnter unmountOnExit- aside should only be added to dom when it becomes visible or invisible
+    <CSSTransition
+      in={props.show}
+      timeout={200}
+      classNames='slide-in-left'
+      mountOnEnter
+      unmountOnExit
+    >
+      <aside className='side-drawer' onClick={props.onClick}>
+        {props.children}
+      </aside>
+    </CSSTransition>
+  );
   return ReactDOM.createPortal(content, document.getElementById("drawer-hook"));
 };
-
 export default SideDrawer;
